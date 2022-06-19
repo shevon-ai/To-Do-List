a9def918a2ebee7a48db0cf03018914f1587bdb8
shevon-ai <65436501+shevon-ai@users.noreply.github.com>
2022-06-19 13:02:32 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const port = 3000;
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+let items = [];
+let workItems = [];
+
+
+app.get("/", function(req, res){
+  let day = date();
+  res.render("list", {listTitle: day, newListItems: items});
+
+});
+
+app.post("/", function(req, res){
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+
+});
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
+
+app.listen(port, function(){
+  console.log("Server is running on port " + port);
+});
